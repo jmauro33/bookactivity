aaf7cec6d6d8ac040f56650c639687aa031637bc
jmauro33 <jonathanmauro16@yahoo.com>
2020-05-05 15:06:38 -0500

    setting up route for app

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,7 @@ import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import SearchBooks from "./pages/SearchBooks";
 import SavedBooks from "./pages/SavedBooks";
-
+import NoMatch from "./pages/NoMatch";
 import Navbar from "./components/Navbar";
 
 function App() {
