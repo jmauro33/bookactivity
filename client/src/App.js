f1ba4381ac338ae6cca8385383c9821c509d3b6b
jmauro33 <jonathanmauro16@yahoo.com>
2020-05-09 14:24:06 -0500

    Fixed capitalization issue

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,7 @@ import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import SearchBooks from "./pages/SearchBooks";
 import SavedBooks from "./pages/SavedBooks";
-import NoMatch from "./pages/NoMatch";
+import NoMatch from "./pages/noMatch";
 import Nav from "./components/Nav";
 
 function App() {
